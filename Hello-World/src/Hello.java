7d4b88add5423b45bfb12f67c7b30cea2fa1a798
KVISDAWONER <krijen16@student.aau.dk>
2021-04-16 19:58:01 +0200

    Scanner hello world + gitignore

--- Hello-World/src/Hello.java
+++ Hello-World/src/Hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name!");
+        String name = scanner.nextLine();
+        System.out.println("Hello" + name + "!");
     }
 }
