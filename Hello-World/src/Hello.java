ae9dd11c4b605ec7d624e88a63badf7c871beed2
KVISDAWONER <krijen16@student.aau.dk>
2021-04-16 20:41:28 +0200

    random number game

--- Hello-World/src/Hello.java
+++ Hello-World/src/Hello.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Hello {
     public static void main(String[] args){
-        Hello();
-        DistToMeters();
-        CreateVehicle();
+        //Hello();
+        //DistToMeters();
+        //CreateVehicle();
+        while(true){
+            NumberGame();
+        }
     }
 
     public static void Hello(){
@@ -27,6 +31,20 @@ public class Hello {
         System.out.println("Created vehicle!");
         System.out.printf("Passengers: %d ",  vehicle.passengers);
         System.out.printf("Fuel: %f ", vehicle.fuel);
-        System.out.printf("Mile per gal: %f", vehicle.mpg);
+        System.out.printf("Mile per gal: %f\n", vehicle.mpg);
+    }
+
+    public static void NumberGame(){
+        System.out.println("Guess a random number beween 1 and 10:");
+
+        //+1 because the max bound is exclusive
+        int rand = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+
+        Scanner scanner = new Scanner(System.in);
+        int guess = Integer.parseInt(scanner.nextLine());
+        if(guess == rand)
+            System.out.println("Congrats you guessed it! It was: " + rand);
+        else
+            System.out.println("Sorry! It was: " + rand);
     }
 }
