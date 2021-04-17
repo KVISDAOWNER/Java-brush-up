2f5563601bce31b32ed5ded874fb7dbc4199fe60
KVISDAWONER <krijen16@student.aau.dk>
2021-04-17 22:38:22 +0200

    Planet: constructor and get resource

--- TwilightImperium/src/Planet.java
+++ TwilightImperium/src/Planet.java
@@ -1,10 +1,19 @@
 public enum Planet {
-    VELNOR,
-    MIRAGE,
-    PERIMETER,
-    VEGAMINOR,
-    VEGAMAJOR,
-    HOPESEND,
-    RIGELII,
-    INDUSTREX
+    VELNOR(1),
+    MIRAGE(1),
+    PERIMETER(1),
+    VEGAMINOR(1),
+    VEGAMAJOR(1),
+    HOPESEND(1),
+    RIGELII(1),
+    INDUSTREX(1);
+
+    private final int production;
+    Planet(int production){
+        this.production = production;
+    }
+
+    int getResource(){
+        return production;
+    }
 }
