a674c658af95198bb74c294556e18715d669697b
KVISDAWONER <krijen16@student.aau.dk>
2021-04-18 22:31:58 +0200

    made ShipType properties package-private

--- TwilightImperium/src/ShipType.java
+++ TwilightImperium/src/ShipType.java
@@ -4,10 +4,10 @@ public enum ShipType {
     CARRIER(3, 9, 1, 6),
     DREADNOUGHT(5, 5, 1, 0);
 
-    public final int res_cost;
-    public final int combat_value;
-    public final int move_speed;
-    public final int capacity;
+    final int res_cost;
+    final int combat_value;
+    final int move_speed;
+    final int capacity;
 
     ShipType(int res_cost, int combat_value, int move_speed, int capacity) {
         this.res_cost = res_cost;
